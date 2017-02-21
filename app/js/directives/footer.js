f739170c296816bc581052c37064bfe9149dd5eb
jimmy <jiangjianming_m@163.com>
2017-02-21 16:22:29 +0800

    add common service

--- app/js/directives/footer.js
+++ app/js/directives/footer.js
@@ -2,27 +2,24 @@
  * Created by jimmy on 16/9/11.
  */
 define(['app'],function(app){
-    app.directive('footer',['$http','$scope',function($http,$scope){
+    app.directive('footer',['commonService',function(commonService){
         return {
             templateUrl: '../app/views/include/footer.html',
             restrict: 'A',
             replace:true,
             link:function($scope,$element,$attrs){
-                //var url={
-                //    baseUrlApi:'http://www.91banke.com/bankehome/',
-                //};
-                //$http.get(url + 'reports')
-                //    .success(function (res, status, headers, config) {
-                //        if (res.status = 'success') {
-                //            var footerNavs = res.data.length;
-                //            $scope.footerNavs = footerNavs;
-                //        }
-                //    });
+                var allParas = {
+                    url: window.globalObj.restUrl + 'reports',
+                    type: 'get',
+                    callback: function (result) {
+                        if (res.status = 'success') {
+                            var footerNavs = res.data.length;
+                            $scope.footerNavs = footerNavs;
+                        }
+                    }
+                };
+                commonService.getData(allParas);
             }
         }
     }]);
-
-
-
-
 });
