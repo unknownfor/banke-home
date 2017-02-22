604732d788853674f9d33d56f996b5816dcb5c0c
jimmy <jiangjianming_m@163.com>
2017-02-22 11:11:14 +0800

    modified header selected status

--- app/js/router.js
+++ app/js/router.js
@@ -19,46 +19,25 @@ define('router',[
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
         $rootScope.$on('$stateChangeStart',function(event,toState,toParams,fromState,fromParams) {
-            //$rootScope.showRegisterBtn = true; //显示注册按钮
-            //$rootScope.isHasBg=false;  //不使用灰色背景
             $rootScope.footerFlag=true;  //
-            //var name=toState.name;
-            //if (name == 'login' || name == 'register') {
-            //    return;
-            //}
-            //var cookie=$cookieStore.get("kunmingPower");
-            //if(!cookie){
-            //    event.preventDefault(); //取消默认跳转行为
-            //    $state.go('login',{from:fromState.name,w:'notLogin'}); //跳转到登录界面
-            //    return;
-            //}
-            //switch (name){
-            //    case 'desktop':
-            //    case 'desktop.my':
-            //        $rootScope.desktopMenuSelectedIndex=0;
-            //        break;
-            //    case 'desktop.achieves':
-            //        $rootScope.desktopMenuSelectedIndex=1;
-            //        break;
-            //    case 'desktop.template':
-            //        $rootScope.desktopMenuSelectedIndex=2;
-            //        break;
-            //    case 'newuser':
-            //        $rootScope.subTopBannerTitle='新用户';
-            //        if(JSON.parse(cookie).type!=0){
-            //            $location.path('/forbidden').replace();  //上一条浏览记录被替换而不是创建一个新的历史记录,
-            //            return;
-            //        }
-            //        break;
-            //    case 'account':
-            //        $rootScope.subTopBannerTitle='账户设置';
-            //        break;
-            //    case 'docdetail':
-            //        $rootScope.footerFlag=false;
-            //        break;
-            //    default :
-            //        break;
-            //}
+            var name=toState.name,menuIndex=0;
+            switch (name){
+                case 'home':
+                case '':
+                    break;
+
+                case 'org':
+                case 'orgdetail':
+                    menuIndex=1;
+                    break;
+                case 'join':
+                    menuIndex=2;
+                    break;
+                default :
+                    menuIndex=3;
+                    break;
+            }
+            $rootScope.headerMenuSelectedIndex=menuIndex;
             //$rootScope.isHasBg=true;
             //$rootScope.showRegisterBtn=false; //隐藏注册按钮
             //$rootScope.globalUserInfo=JSON.parse(cookie);  //存储全局用户信息
