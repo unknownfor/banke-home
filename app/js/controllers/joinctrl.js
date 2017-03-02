a527e8d382bd010743b269d124b614a9ada280ac
jimmy <jiangjianming_m@163.com>
2017-03-02 14:16:57 +0800

    modified join org style

--- app/js/controllers/joinctrl.js
+++ app/js/controllers/joinctrl.js
@@ -4,7 +4,22 @@
 define([
     'app'
 ], function(app) {
-    app.controller('joinCtrl', ['$scope',function($scope) {
-            // $scope.tips='join';
+    app.controller('joinCtrl', ['$scope','commonService',function($scope,cService) {
+        var allParas = {
+            url: window.globalObj.restUrl + 'orgs',
+            type: 'get',
+            callback: function (result) {
+                var data=result.data.message.data,
+                    len=data.length;
+                for(var i=0;i<len;i++){
+                    var url=data[i].cover.replace(/,.*/,'');
+                    data[i].topCover=url;
+                    data[i].details=data[i].details.replace(/<(\S*?)[^>]*>.*?|<.*? \/>$/g,'').replace(/&nbsp;/g,'');
+
+                }
+                $scope.orgArr = data;
+            }
+        };
+        cService.getData(allParas);
     }]);
 });
