4400ffc2230148c2b7e8ebead3ac782f595e6c08
mayoi <1299824519@qq.com>
2017-03-02 13:39:57 +0800

    修改页面样式bug

--- app/js/controllers/orgctrl.js
+++ app/js/controllers/orgctrl.js
@@ -4,7 +4,14 @@
 define([
     'app'
 ], function(app) {
-    app.controller('orgCtrl', ['$scope',function($scope) {
-            $scope.tips='2017-02-21 18:32:13';
+    app.controller('orgCtrl', ['$scope','commonService',function($scope,commonService) {
+        var allParas = {
+            url: window.globalObj.restUrl + 'orgs',
+            type: 'get',
+            callback: function (result) {
+                $scope.orgUl = result.data.message.data;
+            }
+        };
+        commonService.getData(allParas);
     }]);
 });
