51868df369393cbce7d952d096237bf7681c813e
jimmy <jiangjianming_m@163.com>
2017-03-10 16:11:41 +0800

    add banke mobile site

--- app/js/router.js
+++ app/js/router.js
@@ -21,6 +21,12 @@ define('router',[
         $rootScope.$on('$stateChangeStart',function(event,toState,toParams,fromState,fromParams) {
             $rootScope.footerFlag=true;  //
             var name=toState.name,menuIndex=0;
+            if(getDeviceType().mobile){
+                if(name.indexOf('m')!=0) {
+                    $location.path('/mhome').replace();  //
+                }
+                return;
+            }
             switch (name){
                 case 'home':
                 case '':
@@ -145,8 +151,54 @@ define('router',[
                         templateUrl:'views/partner.html'
                     },
                 }
+            })
+
+            //移动端
+            .state('mhome',{
+                url:'/mhome',
+                views:{
+                    '':{
+                        templateUrl:'mobile/views/index.html'
+                    },
+                    'headerView@mhome':{
+                        templateUrl:'mobile/views/include/header.html'
+                    },
+                    'mainView@mhome':{
+                        templateUrl:'mobile/views/home.html'
+                    },
+                }
+            })
+            .state('mjoin',{
+                url:'/mjoin',
+                views:{
+                    '':{
+                        templateUrl:'mobile/views/index.html'
+                    },
+                    'headerView@mjoin':{
+                        templateUrl:'mobile/views/include/header.html'
+                    },
+                    'mainView@mjoin':{
+                        templateUrl:'mobile/views/join.html'
+                    },
+                }
             });
     });
+
+    function getDeviceType(){
+        var u = navigator.userAgent, app = navigator.appVersion;
+        return { //移动终端浏览器版本信息
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+            iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, //是否iPad
+            webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+        };
+    }
 
 });
  
