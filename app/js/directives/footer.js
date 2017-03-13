f4cfb48b98d8cae58dcdba37f6585ee51e558df7
mayoi <1299824519@qq.com>
2017-03-13 15:45:57 +0800

    modified footer style

--- app/js/directives/footer.js
+++ app/js/directives/footer.js
@@ -22,4 +22,25 @@ define(['app'],function(app){
 
 
     }]);
+
+    app.directive('mfooter',['commonService',function(commonService){
+        return {
+            templateUrl: 'mobile/views/include/footer.html',
+            restrict: 'A',
+            replace:true,
+            link:function($scope,$element,$attrs){
+                $scope.year=new Date();
+                var allParas = {
+                    url: window.globalObj.restUrl + 'reports',
+                    type: 'get',
+                    callback: function (result) {
+                        $scope.footerNavs = result.data.message.data;
+                    }
+                };
+                commonService.getData(allParas);
+            }
+        }
+
+
+    }]);
 });
