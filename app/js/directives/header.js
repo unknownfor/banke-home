c396c1908e1b853ed4fdf5b594c7de029d7d4d33
mayoi <1299824519@qq.com>
2017-03-27 16:04:14 +0800

    修改导航栏

--- app/js/directives/header.js
+++ app/js/directives/header.js
@@ -2,28 +2,16 @@
  * Created by hisihi on 2017/3/27.
  */
 define(['app'],function(app) {
-    app.directive('header', ['$scope','commonService', function ($scope,cService) {
-        var allParas = {
-            url: window.globalObj.restUrl + 'header',
-            type: 'get',
-            callback: function (result) {
-                var data=result.data.message.data,
-                    len=data.length;
-                for(var i=0;i<len;i++){
-                    var url=data[i].cover.replace(/,.*/,'');
-                    data[i].topCover=url;
-                    data[i].details=data[i].details.replace(/<(\S*?)[^>]*>.*?|<.*? \/>$/g,'').replace(/&nbsp;/g,'');
-                }
-                $scope.orgArr = data;
-            }
-        };
-        cService.getData(allParas);
-
-
-
+    app.controller('header', ['$scope', function ($scope) {
         $scope.visible=false;
-        $scope.controlUlBox=function(flag){
-            $scope.visible=flag;
+        $scope.menus=[
+            {url:'mhome',title:'首页'},
+            {url:'morg',title:'精选机构'},
+            {url:'mjoin',title:'机构入驻'},
+            {url:'mpartner',title:'城市合伙人'}
+            ];
+        $scope.controlUlBox=function(){
+            $scope.visible=!$scope.visible;
         }
 
     }]);
